d43204577487bcf0a550f1c9833951e2cd82d0af
Juan Manuel Luque <94568781+juanmluq@users.noreply.github.com>
2024-03-06 21:00:55 -0300

    Update Invertir.jsx

--- src/components/Invertir.jsx
+++ src/components/Invertir.jsx
@@ -4,104 +4,98 @@ import { Link } from "react-router-dom";
 
 export default function Invertir(){
 
-    return(
-    <div className="centrPrinc">
-        
+  return(
+    <div className="centrPrinc"> 
         <div className='centr'>
-      <a style={{ width:"auto"}} href="https://api.whatsapp.com/send?phone=+5491124671581">  
-         <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-whatsapp" viewBox="0 0 16 16">
-           <path d="M13.601 2.326A7.854 7.854 0 0 0 7.994 0C3.627 0 .068 3.558.064 7.926c0 1.399.366 2.76 1.057 3.965L0 16l4.204-1.102a7.933 7.933 0 0 0 3.79.965h.004c4.368 0 7.926-3.558 7.93-7.93A7.898 7.898 0 0 0 13.6 2.326zM7.994 14.521a6.573 6.573 0 0 1-3.356-.92l-.24-.144-2.494.654.666-2.433-.156-.251a6.56 6.56 0 0 1-1.007-3.505c0-3.626 2.957-6.584 6.591-6.584a6.56 6.56 0 0 1 4.66 1.931 6.557 6.557 0 0 1 1.928 4.66c-.004 3.639-2.961 6.592-6.592 6.592zm3.615-4.934c-.197-.099-1.17-.578-1.353-.646-.182-.065-.315-.099-.445.099-.133.197-.513.646-.627.775-.114.133-.232.148-.43.05-.197-.1-.836-.308-1.592-.985-.59-.525-.985-1.175-1.103-1.372-.114-.198-.011-.304.088-.403.087-.088.197-.232.296-.346.1-.114.133-.198.198-.33.065-.134.034-.248-.015-.347-.05-.099-.445-1.076-.612-1.47-.16-.389-.323-.335-.445-.34-.114-.007-.247-.007-.38-.007a.729.729 0 0 0-.529.247c-.182.198-.691.677-.691 1.654 0 .977.71 1.916.81 2.049.098.133 1.394 2.132 3.383 2.992.47.205.84.326 1.129.418.475.152.904.129 1.246.08.38-.058 1.171-.48 1.338-.943.164-.464.164-.86.114-.943-.049-.084-.182-.133-.38-.232z"/>
-         </svg>   
-       </a>
-       </div>
-
+          <a style={{ width:"auto"}} href="https://api.whatsapp.com/send?phone=+5491124671581">  
+            <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-whatsapp" viewBox="0 0 16 16">
+              <path d="M13.601 2.326A7.854 7.854 0 0 0 7.994 0C3.627 0 .068 3.558.064 7.926c0 1.399.366 2.76 1.057 3.965L0 16l4.204-1.102a7.933 7.933 0 0 0 3.79.965h.004c4.368 0 7.926-3.558 7.93-7.93A7.898 7.898 0 0 0 13.6 2.326zM7.994 14.521a6.573 6.573 0 0 1-3.356-.92l-.24-.144-2.494.654.666-2.433-.156-.251a6.56 6.56 0 0 1-1.007-3.505c0-3.626 2.957-6.584 6.591-6.584a6.56 6.56 0 0 1 4.66 1.931 6.557 6.557 0 0 1 1.928 4.66c-.004 3.639-2.961 6.592-6.592 6.592zm3.615-4.934c-.197-.099-1.17-.578-1.353-.646-.182-.065-.315-.099-.445.099-.133.197-.513.646-.627.775-.114.133-.232.148-.43.05-.197-.1-.836-.308-1.592-.985-.59-.525-.985-1.175-1.103-1.372-.114-.198-.011-.304.088-.403.087-.088.197-.232.296-.346.1-.114.133-.198.198-.33.065-.134.034-.248-.015-.347-.05-.099-.445-1.076-.612-1.47-.16-.389-.323-.335-.445-.34-.114-.007-.247-.007-.38-.007a.729.729 0 0 0-.529.247c-.182.198-.691.677-.691 1.654 0 .977.71 1.916.81 2.049.098.133 1.394 2.132 3.383 2.992.47.205.84.326 1.129.418.475.152.904.129 1.246.08.38-.058 1.171-.48 1.338-.943.164-.464.164-.86.114-.943-.049-.084-.182-.133-.38-.232z"/>
+            </svg>   
+          </a>
+        </div>
         <div className="inv">
             <h2>Invertir </h2>
             {/* <img className="imginfo" src='https://www.laopinionsemanario.com.ar/wp-content/uploads/2022/03/casino-magic-poker-en-vivo.jpg' alt='img'></img> */}
 
-            <h7>   Casino Diamante LQ te da la posibilidad de que ganes dinero con nosotros.
-                <br />
-                    Si queres ganar dinero de manera segura nosotros te damos la oportunidad.
-                <br />
-                    Ingresas un capital a nuestro casino y nosotros te lo duplicamos en creditos. Es decir, ingresas $5.000 y te damos $10.000
-                    en creditos para que los ofrezcas a amigos, familiares o conocidos. Si no vendes los creditos y queres jugar para probar 
-                    suerte podes hacerlo! 
+                <h7>   Casino Diamante LQ te da la posibilidad de que ganes dinero con nosotros.
                     <br />
-                    La mitad de los creditos son en bonos y el resto es retirable. 
+                        Si queres ganar dinero de manera segura nosotros te damos la oportunidad.
                     <br />
-                    En el caso de jugar el minimo de ganancia para retirar es el 20% de lo depositado, siguiendo el ejemplo anterior retirarias
-                     $6.000 como minimo.
-                     <br />
-                    En el caso de no jugar y no vender podes retirar el 80% de lo depositado. 
-                    <br />
-                    El minimo para empezar a invertir son $2000.
-                    <br />
-                    Si queres comenzar te invitamos a que nos escribas por whatsApp o correo electronico.
-            </h7>
-            <br />
-            <br />
+                        Ingresas un capital a nuestro casino y nosotros te lo duplicamos en creditos. Es decir, ingresas $5.000 y te damos $10.000
+                        en creditos para que los ofrezcas a amigos, familiares o conocidos. Si no vendes los creditos y queres jugar para probar 
+                        suerte podes hacerlo! 
+                        <br />
+                        La mitad de los creditos son en bonos y el resto es retirable. 
+                        <br />
+                        En el caso de jugar el minimo de ganancia para retirar es el 20% de lo depositado, siguiendo el ejemplo anterior retirarias
+                        $6.000 como minimo.
+                        <br />
+                        En el caso de no jugar y no vender podes retirar el 80% de lo depositado. 
+                        <br />
+                        El minimo para empezar a invertir son $2000.
+                        <br />
+                        Si queres comenzar te invitamos a que nos escribas por whatsApp o correo electronico.
+                </h7>
+              <br />
+              <br />
 
-            <h2>Ganancias 2.4 </h2>
+              <h2>Ganancias 2.4 </h2>
 
-            <h7>   Casino Diamante LQ te da la posibilidad de que ganes dinero con Ganancias 2.4.
-                <br />
-                    Si queres ganar dinero de manera segura nosotros te damos la oportunidad.
-                <br />
-                    Con Ganancias 2.4 ingresas un capital a nuestro casino y nosotros te lo multiplicamos por 2.4 en bonos para que los ofrezcas.
-                    <br />
-                    Para ganar tenes que tener $5.000 como minimo y conocer 2 o 3 personas que tambien quieran ganar.  
-                    <br />
-                    El mecanismo es el siguiente: 
-                    <br />
-                    * Ingresas con $5000.
-                    <br />
-                    * En tu cuenta se van a ver acreditados $15.000 en bonos.
-                    <br />
-                    * Vos tenes que vender 3 bonos de $5.000 de los cuales cada persona que compra va a ver acreditado $15.000.
-                    <br />
-                    * De esos $5.000 que te pagó vos te quedas con $4.000 y a nosotros nos depositas los restantes $1.000.
-                    <br />
-                    * Para que entiendas mejor ahora que hiciste tu primera venta vos tenes $4.000 en efectivo y $10.000 en bonos. Por otro lado el cliente 
-                    que te compro tiene $15.000 en bonos para vender.
-                    <br />
-                    * Luego de que vos hayas vendido los 3 bonos de $5.000 tendras en tu bolsillo $12.000 en efectivo. 
-                    <br />
-                    Los bonos no son retirables. Si no queres vender y los queres usar para jugar se puede pero solo se acredita
-                     un 80% de lo que depositaste. La idea de ganancias 2.4 es vender para multiplicar tu dinero.
-                     <br />
-                    El minimo para empezar a invertir son $5000, pero podes elegir el monto que vos quieras hasta $30.000.
-                    <br />
-                    Con $5.000 podes llevarte $12.000
-                    <br />
-                    Con $10.000 podes llevarte $24.000
-                    <br />
-                    Con $30.000 podes llevarte $72.000
-                    <br />
-                    Si queres comenzar te invitamos a que nos escribas por whatsApp o correo electronico.
-            </h7>
-            <br />
-            <h6>Acceder a Home Ganancias 2.4:</h6>
-            <Link to="/acceder" class="btn" >Ingresar</Link>
-            <br />
-            <br />
+              <h7>   Casino Diamante LQ te da la posibilidad de que ganes dinero con Ganancias 2.4.
+                  <br />
+                      Si queres ganar dinero de manera segura nosotros te damos la oportunidad.
+                  <br />
+                      Con Ganancias 2.4 ingresas un capital a nuestro casino y nosotros te lo multiplicamos por 2.4 en bonos para que los ofrezcas.
+                      <br />
+                      Para ganar tenes que tener $5.000 como minimo y conocer 2 o 3 personas que tambien quieran ganar.  
+                      <br />
+                      El mecanismo es el siguiente: 
+                      <br />
+                      * Ingresas con $5000.
+                      <br />
+                      * En tu cuenta se van a ver acreditados $15.000 en bonos.
+                      <br />
+                      * Vos tenes que vender 3 bonos de $5.000 de los cuales cada persona que compra va a ver acreditado $15.000.
+                      <br />
+                      * De esos $5.000 que te pagó vos te quedas con $4.000 y a nosotros nos depositas los restantes $1.000.
+                      <br />
+                      * Para que entiendas mejor ahora que hiciste tu primera venta vos tenes $4.000 en efectivo y $10.000 en bonos. Por otro lado el cliente 
+                      que te compro tiene $15.000 en bonos para vender.
+                      <br />
+                      * Luego de que vos hayas vendido los 3 bonos de $5.000 tendras en tu bolsillo $12.000 en efectivo. 
+                      <br />
+                      Los bonos no son retirables. Si no queres vender y los queres usar para jugar se puede pero solo se acredita
+                      un 80% de lo que depositaste. La idea de ganancias 2.4 es vender para multiplicar tu dinero.
+                      <br />
+                      El minimo para empezar a invertir son $5000, pero podes elegir el monto que vos quieras hasta $30.000.
+                      <br />
+                      Con $5.000 podes llevarte $12.000
+                      <br />
+                      Con $10.000 podes llevarte $24.000
+                      <br />
+                      Con $30.000 podes llevarte $72.000
+                      <br />
+                      Si queres comenzar te invitamos a que nos escribas por whatsApp o correo electronico.
+              </h7>
+              <br />
+              <h6>Acceder a Home Ganancias 2.4:</h6>
+              <Link to="/acceder" class="btn" >Ingresar</Link>
+              <br />
+              <br />
 
-            <h2>Hacete cajero </h2>
-            {/* <img className="imginfo" src='https://www.laopinionsemanario.com.ar/wp-content/uploads/2022/03/casino-magic-poker-en-vivo.jpg' alt='img'></img> */}
+              <h2>Hacete cajero </h2>
+              {/* <img className="imginfo" src='https://www.laopinionsemanario.com.ar/wp-content/uploads/2022/03/casino-magic-poker-en-vivo.jpg' alt='img'></img> */}
 
-            <h7>   Casino Diamante LQ incorpora cajeros!
-                <br />
-                    Ofrecemos la mejor ganancia del mercado!
-                <br />
-                    Si queres trabajar con nosotros te invitamos a que nos escribas por whatsApp o correo electronico.
-            </h7>              
-            <br />
-            <br />
-            <br />
-            <br />
-
-          
-
-
+              <h7>   Casino Diamante LQ incorpora cajeros!
+                  <br />
+                  Ofrecemos la mejor ganancia del mercado!
+                  <br />
+                  Si queres trabajar con nosotros te invitamos a que nos escribas por whatsApp o correo electronico.
+              </h7>              
+              <br />
+              <br />
+              <br />
+              <br />
                     {/* Gana hasta el 400% de lo que vendas.
                     Te ofrecemos creditos al 25% de su valor.
                     Vos depositas $2.500 y nosotros te damos $10.000 en creditos los cuales podes vender a tus clientes.
@@ -110,6 +104,6 @@ export default function Invertir(){
                     Tu ganancia seria del 400% si vendes todo al valor normal, siguiendo el ejemplo vos ganarias $7.500. 
                     En el caso que el cliente gane vos le pagas y nosotros te ingresamos esos creditos a tu cuenta para que puedas volver a vender.    */}
         </div>
-        </div>
-    )
+      </div>
+  )
 }
